ebcc0c1c50f98626f5570db1d6ec95708913cf5b
BorisMilanov <68430756+BorisMilanov@users.noreply.github.com>
2023-01-26 14:17:26 +0200

    Config controller 0.1

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,9 +1,21 @@
+const  User = require('../models/userModel');
+
 const loginUser = async (req, res) => {
     res.json({ mssg: 'login user' });
 }
 
 const signupUser = async (req, res) => {
-    res.json({ mssg: 'signup user' });
+    const {email, password} = req.body;
+
+    try {
+     const user = await User.signup(email,password)
+        
+    } catch (error) {
+        console.log(error);
+    }
+
 }
+
+
 
 module.exports = { signupUser, loginUser }
